88e5064a8eec610f137082066ae1c573da2bbe88
anishghimire <anishghimire862@gmail.com>
2020-03-21 16:21:17 +0545

    Added server.js file

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+require('./app/config/passport');
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
+});
+
+var routes = require("./app/routes/routes.js");
+app.use(routes);
+
+module.exports = app
