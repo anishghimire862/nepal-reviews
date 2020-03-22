93d6b7ca6e40c5e1238107e99a42aeb7d8af3807
anishghimire <anishghimire862@gmail.com>
2020-03-22 16:10:38 +0545

    Test environment checked while starting server

--- server.js
+++ server.js
@@ -21,9 +21,11 @@ db.sequelize.sync();
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}.`);
-});
+if(process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);
+  });
+}
 
 var routes = require("./app/routes/routes.js");
 app.use(routes);
