c9df2500cff38b05e5ca558851c0ad6e82dcdbb5
anishghimire <anishghimire862@gmail.com>
2020-04-01 17:49:23 +0545

    Updated url for CORS

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('./app/config/passport');
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
