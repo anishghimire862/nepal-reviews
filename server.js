7fa0ff000baa6916bbd898b5857e94aa7626590e
Anish Ghimire <anishghimire862@gmail.com>
2020-06-04 21:25:27 +0545

    Swagger docs api set

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ if(process.env.NODE_ENV !== 'test') {
 }
 
 var routes = require("./app/routes/routes.js");
+var swaggerUi = require('swagger-ui-express'),
+  swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(routes);
 
 module.exports = app;
