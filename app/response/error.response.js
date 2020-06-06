0f994e7ae04fdb5948c9f9de72c59f2a1d31488e
Anish Ghimire <anishghimire862@gmail.com>
2020-06-06 22:37:12 +0545

    Added responses

---
+++ app/response/error.response.js
@@ -0,0 +1,7 @@
+module.exports = {
+  INVALID_LOGIN_DETAILS: "Incorrect email/username or password.",
+  EMAIL_ALREADY_EXISTS: "Email address already in use.",
+  USERNAME_ALREADY_EXISTS: "Username already in use.",
+  UNAUTHORIZED: "Sorry! you are not authorized to perform this operation.",
+  REVIEW_ALREADY_EXISTS: "You have already reviewed this Thread."
+}
